612b1cc5fd98be5e2fe5c518d75dda7287a90b8d
Cornel Barna <cornel.barna@senecacollege.ca>
2025-06-10 10:30:04 -0400

    Week #4, Exercise #1 solution has been released!

---
+++ exercises/week-04/exercise-01/solution/shoppingCart.h
@@ -0,0 +1,30 @@
+// shoppingCart.h
+// 2021/12/02 - Miguel
+
+#ifndef SENECA_SHOPPINGCART_H
+#define SENECA_SHOPPINGCART_H
+
+#include <ostream>
+#include "toy.h"
+
+namespace seneca
+{
+	//manages a collection of toys
+	class ShoppingCart
+	{
+		std::string m_childName{};
+		int m_childAge{};
+		const seneca::Toy** m_pToys{};
+		size_t m_count{};
+	public:
+		ShoppingCart(const std::string& name, int age, const Toy* toys[], size_t count);
+		ShoppingCart(const ShoppingCart& other);
+		ShoppingCart& operator=(const ShoppingCart& other);
+		ShoppingCart(ShoppingCart&& other) noexcept;
+		ShoppingCart& operator=(ShoppingCart&& other) noexcept;
+		~ShoppingCart();
+
+		friend std::ostream& operator<<(std::ostream& os, const ShoppingCart& cart);
+	};
+}
+#endif// SENECA_SHOPPINGCART_H
