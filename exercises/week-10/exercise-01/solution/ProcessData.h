689365d8d1d1f44e62f7cc4a17283b355bcd52c2
Michael Huang <joulecpa@gmail.com>
2025-07-26 21:57:49 -0400

    Week9/10 exercise solution

---
+++ exercises/week-10/exercise-01/solution/ProcessData.h
@@ -0,0 +1,35 @@
+// Exercise - Multi-Threading, Thread Class
+// 2021/1/5 - Jeevan Pant
+// 2024/5/10 - Cornel
+
+#ifndef SENECA_PROCESSDATA_H
+#define SENECA_PROCESSDATA_H
+
+#include <string>
+
+namespace seneca
+{
+	void computeAvgFactor(const int*, int, int, double&);
+	void computeVarFactor(const int*, int, int, double, double&);
+
+	class ProcessData
+	{
+		int total_items{};
+		int* data{};
+		// Following parameters are included to be used for managing multi-threaded 
+		//  computation in the operator() function. 
+		int num_threads{ 0 }; // to hold number of threads 
+		double* averages{ nullptr }; // to hold average factors 
+		double* variances{ nullptr }; // to hold variance factors
+		int* p_indices{ nullptr }; // to hold partitioning indices
+	public:
+		ProcessData(const std::string&, int n_threads);
+		ProcessData(const ProcessData&) = delete;
+		ProcessData& operator=(const ProcessData&) = delete;
+		~ProcessData();
+		operator bool() const;
+		int operator()(const std::string&, double&, double&);
+	};
+}
+
+#endif
