05549c34c132964b1be86c9ba4f82a9469af2e67
Cornel Barna <cornel.barna@senecacollege.ca>
2026-02-06 13:02:47 -0500

    Solution for Week #4, Exercise #1 has been released!

---
+++ exercises/week-04/exercise-01/solution/confirmationOrder.h
@@ -0,0 +1,29 @@
+// ConfirmationOrder.h - class declaration for a confirmation order system
+// 2021/12/02 - Miguel
+
+#ifndef SENECA_CONFIRMATIONORDER_H
+#define SENECA_CONFIRMATIONORDER_H
+#include <ostream>
+#include "toy.h"
+
+namespace seneca
+{
+	class ConfirmationOrder
+	{
+		const seneca::Toy** m_pToys{};
+		size_t m_count{};
+	public:
+		ConfirmationOrder() = default;
+		~ConfirmationOrder();
+		ConfirmationOrder(const ConfirmationOrder& other);
+		ConfirmationOrder& operator=(const ConfirmationOrder& other);
+		ConfirmationOrder(ConfirmationOrder&& other) noexcept;
+		ConfirmationOrder& operator=(ConfirmationOrder&& other) noexcept;
+
+		ConfirmationOrder& operator+=(const Toy& toy);
+		ConfirmationOrder& operator-=(const Toy& toy);
+
+		friend std::ostream& operator<<(std::ostream& os, const ConfirmationOrder& order);
+	};
+}
+#endif// SENECA_CONFIRMORDER_H
